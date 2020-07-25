695f7475f2c12443a4eb6de8a6ca9762f8fb53a0
Louiks <kokot.jakub@wp.pl>
2020-07-26 00:37:54 +0200

    Player movement and camera attached

--- Assets/MovementManager.cs
+++ Assets/MovementManager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class MovementManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float Speed =10;
+    public float JumpSpeed =1;
+    public Rigidbody rigidbody;
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Jump();
+        Vector3 playerMovement = new Vector3(horizontal, 0, vertical).normalized * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+
+    void Jump(){
+        if(Input.GetAxis("Jump")>0) { 
+            rigidbody.AddForce(Vector3.up *JumpSpeed);
+        }
     }
 }
